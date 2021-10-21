b558ab036ef8551f52aca892a74c22017ba0c31f
kaloresanket <kaloresanket@gmail.com>
2021-10-21 10:45:46 +0000

    Fix additional denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,6 +145,8 @@ VENDOR_SECURITY_PATCH := 2021-05-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
