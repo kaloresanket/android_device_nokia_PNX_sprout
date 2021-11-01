af51f6d5e2f6ad0f0a9df286194c30905a7a59cf
kaloresanket <67557993+kaloresanket@users.noreply.github.com>
2021-11-01 10:08:09 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -191,10 +191,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1 \
-    android.hardware.biometrics.fingerprint@2.1-service
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
+    android.hardware.biometrics.fingerprint@2.1-service.nokia_sdm710
 
 # GPS
 PRODUCT_PACKAGES += \
