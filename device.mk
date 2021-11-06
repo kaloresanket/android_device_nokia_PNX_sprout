1fada7cc49dc59987acdce5be4cf611b2bea7abd
kaloresanket <67557993+kaloresanket@users.noreply.github.com>
2021-11-06 12:54:32 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -234,6 +234,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1 \
     android.hardware.biometrics.fingerprint@2.1-service.nokia_sdm710 \
 
 # GPS
